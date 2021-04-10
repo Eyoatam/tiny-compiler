e5b7c7f29d822a6d5c66447266e21a2ee11dc4be
Eyoatam <eyoatamtamirat7@gmail.com>
2021-04-10 10:42:52 +0300

    added transformer and compiler functions

--- src/compiler.ts
+++ src/compiler.ts
@@ -1,7 +1,40 @@
+// deno-lint-ignore-file
+
 export interface Token {
   type: string;
   value: string;
 }
+
+export interface Callee {
+  type: string;
+  name: string;
+}
+
+export interface Context {
+  type: string;
+  value?: string;
+  callee?: { type: string; name: any };
+  arguments?: any[];
+}
+
+export interface Visitor {
+  [index: string]: {
+    enter(node: any, parent: any): void;
+    exit?(node: any, parent: any): void;
+  };
+  NumberLiteral: { enter(node: any, parent: any): void };
+  StringLiteral: { enter(node: any, parent: any): void };
+  CallExpression: { enter(node: any, parent: any): void };
+}
+
+export type Expression = {
+  type: string;
+  callee?: { type: string; name: string };
+  arguments?: string[] | any[];
+  expression: unkObj;
+};
+
+export type unkObj = Record<string, unknown>;
 
 export function tokenize(input: string): Token[] {
   let current = 0;
@@ -75,7 +108,7 @@ export function tokenize(input: string): Token[] {
       continue;
     }
 
-    throw new TypeError("Unknown type" + char);
+    throw new TypeError(`Unknown type ${char}`);
   }
   return tokens;
 }
@@ -109,7 +142,7 @@ export function parse(tokens: Token[]) {
       const node: {
         type: string;
         name: string;
-        params: Record<string, unknown>[];
+        params: unkObj[];
       } = {
         type: "CallExpression",
         name: token.value,
@@ -129,11 +162,10 @@ export function parse(tokens: Token[]) {
       current++;
       return node;
     }
-    throw new TypeError("Unknown type" + token.type);
+    throw new TypeError(`Unknown type ${token.type}`);
   }
 
-  // deno-lint-ignore no-explicit-any
-  const ast: { type: string; body: any[] } = {
+  const ast: { type: string; body: unkObj[] } = {
     type: "Program",
     body: [],
   };
@@ -145,12 +177,144 @@ export function parse(tokens: Token[]) {
   return ast;
 }
 
-// const parsedTokens = parse(
-//   tokenize(`(add 2 (subtract 4 2))
-// (add 2 2)
-// (subtract 4 2)
-// `)
-// );
-// const stringified = JSON.stringify(parsedTokens);
-// const encoder = new TextEncoder();
-// await Deno.writeFile("./ast.json", encoder.encode(stringified));
+export function traverse(ast: any, visitor: Visitor) {
+  function traverseArray(array: any[], parent: any) {
+    array.forEach((child: any) => {
+      traverseNode(child, parent);
+    });
+  }
+  function traverseNode(
+    node: { type: string; body: any[]; params: any[] },
+    parent: any,
+  ) {
+    let methods = visitor[node.type];
+
+    if (methods && methods.enter) {
+      methods.enter(node, parent);
+    }
+    switch (node.type) {
+      case "Program":
+        traverseArray(node.body, node);
+        break;
+
+      case "CallExpression":
+        traverseArray(node.params, node);
+        break;
+
+      case "NumberLiteral" || "StringLiteral":
+        break;
+
+      default:
+        throw new TypeError(node.type);
+    }
+
+    if (methods && methods.exit) {
+      methods.exit(node, parent);
+    }
+  }
+  traverseNode(ast, null);
+}
+export function transform(ast: {
+  type?: string;
+  body?: unkObj[];
+  _context?: any;
+}) {
+  const newAst: { type: string; body: unkObj[] } = {
+    type: "Program",
+    body: [],
+  };
+
+  ast._context = newAst.body;
+
+  traverse(ast, {
+    NumberLiteral: {
+      enter(
+        node: { value: any },
+        parent: { _context: { type: string; value: any }[] },
+      ) {
+        parent._context.push({
+          type: "NumberLiteral",
+          value: node.value,
+        });
+      },
+    },
+
+    StringLiteral: {
+      enter(node: { value: any }, parent: { _context: Context[] }) {
+        parent._context.push({
+          type: "StringLiteral",
+          value: node.value,
+        });
+      },
+    },
+
+    CallExpression: {
+      enter(
+        node: { name: string; _context: Context[] },
+        parent: {
+          type: string;
+          _context: Context[];
+        },
+      ) {
+        let expression: Expression = {
+          type: "CallExpression",
+          callee: {
+            type: "Identifier",
+            name: node.name,
+          },
+          arguments: [],
+          expression: {},
+        };
+        node._context = expression.arguments ? expression.arguments : [];
+        if (parent.type !== "CallExpression") {
+          expression = {
+            type: "ExpressionStatement",
+            expression,
+          };
+        }
+        parent._context.push(expression);
+      },
+    },
+  });
+  return newAst;
+}
+
+function generate(node: {
+  type?: string;
+  body: any[];
+  expression?: any;
+  callee?: any;
+  arguments?: any[];
+  name?: string;
+  value?: string;
+}): string {
+  switch (node.type) {
+    case "Program":
+      return node.body.map(generate).join("\n");
+    case "ExpressionStatement":
+      return generate(node.expression) + ";";
+    case "CallExpression":
+      return (
+        generate(node.callee) +
+        "(" +
+        node.arguments?.map(generate).join(", ") +
+        ")"
+      );
+    case "Identifier":
+      return node.name ? node.name : "";
+    case "NumberLiteral":
+      return node.value ? node.value : "";
+    case "StringLiteral":
+      return '"' + node.value + '"';
+    default:
+      throw new TypeError(`unknown type ${node.type}`);
+  }
+}
+
+export function compile(input: string): string {
+  const tokens = tokenize(input);
+  const ast = parse(tokens);
+  const newAst = transform(ast);
+  const output = generate(newAst);
+  return output;
+}
