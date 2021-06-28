c003a49aacdcb853b1955d736bffc4d58273ebe1
Eyoatam <eyoatamtamirat7@gmail.com>
2021-06-28 20:15:08 +0300

    upgrade deno_std to 0.99.0

--- tests/compiler_test.ts
+++ tests/compiler_test.ts
@@ -1,7 +1,7 @@
 import {
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.97.0/testing/asserts.ts";
+} from "https://deno.land/std@0.99.0/testing/asserts.ts";
 import {
   compile,
   generate,
